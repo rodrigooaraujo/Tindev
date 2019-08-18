5a6c1eddffb03572d5491d122f9a92bc5ccc678a
Rodrigo Araujo <rodrigooaraujo@live.com>
2019-08-18 15:39:12 +1000

    Improved layout, implemented like, dislike, refresh list

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+import api from '../services/api';
+
+export default function Login({ history }) {
+
+    const [userName, setUserName] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username: userName
+        });
+
+        const { _id: id } = response.data;
+        console.log(id);
+
+        history.push(`/dev/${id}`);
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev" />
-                <input placeholder="Enter your GitHub username" />
+                <input
+                    value={userName}
+                    onChange={e => setUserName(e.target.value)}
+                    placeholder="Enter your GitHub username"
+                />
                 <button type="submit">Send</button>
             </form>
-
         </div>
 
     );
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './main.css';
+
+import api from '../services/api';
+
+import Logo from '../assets/logo.svg';
+import Like from '../assets/like.svg';
+import Dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
-    return <h1>{match.params.id}</h1>
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id,
+                }
+            });
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [match.params.id]);
+
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id }
+        });
+
+        setUsers(users.filter(user => user._id != id));
+    }
+
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id }
+        });
+
+        setUsers(users.filter(user => user._id != id));
+    }
+
+    return (
+        <div className="main-container">
+            <img src={Logo} alt="Tindev" />
+            {users.length > 0 ?
+                (<ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <img src="https://avatars0.githubusercontent.com/u/7944362?v=4" alt={user.name} />
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={Dislike} alt="Dislike" />
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={Like} alt="Like" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>) : (
+                    <div className="empty">Finished</div>
+                )}
+        </div>
+    )
 }
